6028b987a73a2e50add4a758be5018019e2142cf
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-06-14 00:53:53 +0530

    creating a variable to get the value from the event when the data is added into the form named as handleSubmitNewTodo.. created new api function in the file to create and add new todo.. for the constant refresh of the browser using the react router from next navigation..since we were getting error every time while creating new todo due to the same id so we are using uuid..

--- api.ts
+++ api.ts
@@ -5,7 +5,21 @@ import { todo } from "node:test";
 const  baseUrl = 'http://localhost:3001';
 
 export const getAllTodos = async (): Promise<ITask[]> =>{
-    const res = await fetch(`${baseUrl}/task`)
+    const res = await fetch(`${baseUrl}/task`, {cache: 'no-store'})
     const todos = await res.json()
 return todos;
 } 
+
+
+export const addTodo = async (todo : ITask):Promise<ITask> => {
+    const res = await fetch(`${baseUrl}/task`,{
+        method: 'POST',
+        headers:{
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    const newTodo = await res.json();
+
+    return newTodo;
+}
--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,17 +1,30 @@
 'use client';
 
-import React, { FormEvent, FormEventHandler, useState } from 'react'
+import React, { FormEvent, FormEventHandler, useReducer, useState } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
 import Modal from './Modal';
+import { addTodo } from '@/api';
+import { useRouter } from 'next/navigation';
+import { v4 as uuidv4 } from 'uuid';
+
 
 
 const AddTask = () => {
+  const router =useRouter()
+
   const [modalOpen, setModalOpen] = useState<boolean>(false)
+  const [newTaskValue, setNewTaskValue] = useState<string>('');
 
-  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = (e) =>{
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) =>{
     e.preventDefault();
+    await addTodo({
+      id: uuidv4(),
+      text: newTaskValue
+    })
+    setNewTaskValue("")
+    setModalOpen(false)
+    router.refresh();
   }
-
   return (
     <div>
       <button className='btn btn-primary w-full' onClick={() => setModalOpen(true)}>
@@ -22,11 +35,17 @@ const AddTask = () => {
         <form  onSubmit={handleSubmitNewTodo}>
           <h3 className='font-bold text-lg'>Add new Task</h3>
           <div className='modal-action'>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-full" />
+            <input 
+              value={newTaskValue}
+              onChange={e =>setNewTaskValue(e.target.value)} 
+              type="text"
+              placeholder="Type here" 
+              className="input input-bordered w-full max-w-full" 
+            />
           </div>
-          <button type='submit' className='btn '>Submit</button>
+          <button type='submit' className='btn m-2'>Submit</button>
         </form>
-      </Modal>`
+      </Modal>
     </div>
 
   )
